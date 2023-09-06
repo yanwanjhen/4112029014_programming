71d05cfd2aa7cf3e9f4ebcf7df4bdcc894acc872
yanwanjhen <144209764+yanwanjhen@users.noreply.github.com>
2023-09-06 21:23:52 +0800

    Add files via upload

---
+++ spyder.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+height=float(input("請輸入您的身高(公尺)"))
+weight=float(input("請輸入您的體重(公斤)"))
+BMI=weight/(height**2)
+print(f"您的BMI是:{BMI:.2f}")
+
